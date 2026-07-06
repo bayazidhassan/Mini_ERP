9208496c35177a9d5474109a2d172d1f2b81a1fb
bayazidhassan <bayazidhassan776@gmail.com>
2026-07-06 22:16:46 +0600

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl">Mini ERP</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
